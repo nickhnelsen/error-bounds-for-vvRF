8fb4b981b5507abb630caa7891f0abef7833ab4e
nickhnelsen <nnelsen@caltech.edu>
2023-08-04 09:57:20 -0700

    cleans cript

--- job_sweep.sh
+++ job_sweep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=24:00:00
+#SBATCH --time=99:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres gpu:1
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --job-name="sweep"
 #SBATCH --output=R-%x.%j.out
 #SBATCH --error=R-%x.%j.err
@@ -15,8 +15,8 @@
 #SBATCH --mail-type=FAIL
 
 declare -a Js=("512" "2048" "8192")
-declare -a Ns=("10" "100" "500" "1000" "1900")
-declare -a Ms=("10" "100" "500" "1000" "1900" "5000" "10000")
+declare -a Ns=("10" "23" "54" "124" "288" "668" "1548")
+declare -a Ms=("10" "32" "100" "316" "1000" "3162" "10000")
 
 conda activate operator
 
@@ -26,7 +26,7 @@ for N in "${Ns[@]}"
 do
 for M in "${Ms[@]}"
 do
-    python -u run_sweep_script.py $M $N $J | tee result_J${J}_m${M}_n${N}.out
+    srun python -u run_sweep_script.py $M $N $J | tee result_J${J}_m${M}_n${N}.out
 done
 done
 done
