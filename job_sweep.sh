d1e4d0abc2507072a13c87556c245b05ca559892
nickhnelsen <nnelsen@caltech.edu>
2023-08-05 11:29:51 -0700

    fix mem

--- job_sweep.sh
+++ job_sweep.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-NMC=100
+NMC=10
 declare -a Js=("512" "2048" "8192")
+
+dir_name="./results/"
+mkdir -p ${dir_name}
 
 COUNT=0
 for ((idx=0;idx<NMC;idx++)); do
     for J in "${Js[@]}"; do
         job_name="idx${idx}_J${J}"
-        std="./results/R-%x.%j"
+        std="${dir_name}R-%x.%j"
         scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err sweep_rfm.sbatch ${idx} ${J}"
         
         echo "submit command: $scommand"
